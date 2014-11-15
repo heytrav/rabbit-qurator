e1d59c9669570f336ddf76dca73f01bca88e9da0
Travis Holton <travis@ideegeo.com>
2014-11-16 07:16:13 +1300

    add "self" since it's an instance variable now

--- tests/test_kombu.py
+++ tests/test_kombu.py
@@ -4,20 +4,21 @@ import os
 from kombu import Connection
 import datetime
 
-conn_dict = {
-    'hostname': os.environ['AMQ_PORT_5672_TCP_ADDR'],
-    'port': os.environ['AMQ_PORT_5672_TCP_PORT'],
-    'userid': os.environ['AMQ_USER'],
-    'password': os.environ['AMQ_PASSWORD'],
-    'ssl': False,
-    'virtual_host': os.environ['AMQ_VHOST'],
-}
 
 
 
 class TestKombu(TestCase):
 
     """Test connection behaviour"""
+
+    conn_dict = {
+        'hostname': os.environ['AMQ_PORT_5672_TCP_ADDR'],
+        'port': os.environ['AMQ_PORT_5672_TCP_PORT'],
+        'userid': os.environ['AMQP_USER'],
+        'password': os.environ['AMQP_PASSWORD'],
+        'ssl': False,
+        'virtual_host': os.environ['AMQP_VHOST'],
+    }
 
 
     def test_async_messaging(self):
@@ -29,8 +30,7 @@ class TestKombu(TestCase):
             """Test publisher behaviour
             :returns: TODO
 
-            """
-            with Connection(**conn_dict) as conn:
+            with Connection(**self.conn_dict) as conn:
                 simple_queue = conn.SimpleQueue('simple_queue')
                 message = 'hello world, sent at %s' % datetime.datetime.today()
                 simple_queue.put(message)
@@ -41,9 +41,7 @@ class TestKombu(TestCase):
         def consumer():
             """Test behaviour of basic amqp consumer
 
-            :stop: fn to stop IO loop during test
-            """
-            with Connection(**conn_dict) as conn:
+            with Connection(**self.conn_dict) as conn:
                 simple_queue = conn.SimpleQueue('simple_queue')
                 message = simple_queue.get(block=True, timeout=1)
                 print('Received: %s' % message.payload)
