45957cc2e5a5b42fff746dd231e95cafc92d89cd
Travis Holton <travis@ideegeo.com>
2014-11-17 07:05:58 +1300

    testing rabbit/kombu interaction

--- tests/test_kombu.py
+++ tests/test_kombu.py
@@ -1,15 +1,13 @@
-from unittest import TestCase
 import os
-
-from kombu import Connection
 import datetime
 
+from unittest import TestCase
 
-
+from kombu import Connection
 
 class TestKombu(TestCase):
 
-    """Test connection behaviour"""
+    """Docstring for TestKombu. """
 
     conn_dict = {
         'hostname': os.environ['AMQ_PORT_5672_TCP_ADDR'],
@@ -20,8 +18,11 @@ class TestKombu(TestCase):
         'virtual_host': os.environ['AMQP_VHOST'],
     }
 
+        
 
-    def test_async_messaging(self):
+class SyncTest(TestKombu):
+
+    def test_basic_messaging(self):
         """Test basic producer->consumer behaviour.
         :returns: TODO
 
@@ -51,3 +52,55 @@ class TestKombu(TestCase):
         publisher()
         message = consumer()
         self.assertRegex(message.payload, r'hello', 'Matches regex')
+
+
+class AsyncTest(TestKombu):
+
+    conn_dict = {
+        'hostname': os.environ['AMQ_PORT_5672_TCP_ADDR'],
+        'port': os.environ['AMQ_PORT_5672_TCP_PORT'],
+        'userid': os.environ['AMQP_USER'],
+        'password': os.environ['AMQP_PASSWORD'],
+        'ssl': False,
+        'virtual_host': os.environ['AMQP_VHOST'],
+    }
+
+    def test_async_messaging(self):
+        """Test the ConsumerMixin class """
+        from kombu import Exchange, Queue, Consumer
+        from kombu.pools import producers
+
+        test_exchange = Exchange('rabbitpytests', type='direct')
+        test_queues = [
+            Queue('test1', test_exchange, routing_key='testtask'),
+        ]
+
+        # Start a consumer and wait
+        from threading import Thread
+        def run_consumer():
+            def on_message( body, message):
+                print("Called on_message")
+                message.ack()
+                print("Consumer got message: %r" % body)
+                self.assertRegex(
+                    body['data'], 
+                    r'Hello', 
+                    'Received "Hello, World" from producer.'
+                )
+
+            with Connection(**self.conn_dict) as conn:
+                with Consumer(conn, test_queues, callbacks=[on_message]):
+                    conn.drain_events(timeout=3)
+
+        t = Thread(target=run_consumer)
+        t.start()
+
+        # have producer send a message
+        with Connection(**self.conn_dict) as conn:
+            with producers[conn].acquire(block=True) as producer:
+                payload = {'data': 'Hello, World'}
+                producer.publish(payload,
+                                exchange=test_exchange,
+                                 serializer='json',
+                                declare=[test_exchange],
+                                routing_key='testtask')
