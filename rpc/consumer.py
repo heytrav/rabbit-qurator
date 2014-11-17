6ba166a6bc45cf1aa114ae470378ea2c57bba2cd
Travis Holton <travis@ideegeo.com>
2014-11-17 20:24:57 +1300

    working on worker class

--- rpc/consumer.py
+++ rpc/consumer.py
@@ -1,7 +1,9 @@
 from kombu.mixins import ConsumerMixin
 from kombu.log import get_logger
 from kombu.utils import kwdict, reprcall
-from .queues import queues
+from kombu.pools import producers
+from kombu.common import send_reply
+from .queues import queues, exchange
 
 logger = get_logger(__name__)
 
@@ -22,25 +24,26 @@ class Worker(ConsumerMixin):
         """Worker(connection)
 
         :connection: Connection object
-
         """
         ConsumerMixin.__init__(self)
         self.connection = connection
-        
+
+
     def get_consumers(self, Consumer, channel):
         """Get consumers.
 
-        :Consumer: TODO
-        :channel: TODO
-        :returns: TODO
+        :Consumer: Consumer object
+        :channel: a channel
+        :returns: array of Consumer objects
 
         """
         return [Consumer(queues=queues,
                          accept=['json'],
                          callbacks=[self.process_task])]
 
+
     def process_task(self, body, message):
-        """TODO: Docstring for process_task.
+        """Callback for processing task.
 
         :body: body of message
         :message: message object
@@ -51,7 +54,15 @@ class Worker(ConsumerMixin):
         kwargs = body['kwargs']
         logger.info('Got task: %s', reprcall(fun.__name__, args, kwargs))
         try:
-            fun(*args, **kwdict(kwargs))
+            response = fun(*args, **kwdict(kwargs))
+            with Connection(**conn_dict) as conn:
+                with producers[conn].acquire(block=True) as producer:
+                    send_reply(
+                        exchange,
+                        message,
+                        response,
+                        producer
+                    )
         except Exception as exc:
             logger.error('task raised excetion: %r', exc)
         message.ack()
