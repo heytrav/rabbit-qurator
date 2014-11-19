3e31f67faa8425a09634f40a21676642c649f714
Travis Holton <travis@ideegeo.com>
2014-11-19 15:41:40 +1300

    removed commented out code

--- rpc/exchange.py
+++ rpc/exchange.py
@@ -1,13 +1,3 @@
 from kombu import Exchange
 
 exchange = Exchange('rabbitpy', type='direct')
-#server_queue
-
-#consumer_exchange = Exchange('rabbitpy_server_ex', type='direct')
-#client_exchange = Exchange('rabbitpy_client_ex', type='direct')
-#client_queues = [Queue('client_hello', client_exchange, routing_key='client_hello'),
-                 #Queue('client_version',
-                       #client_exchange,
-                       #routing_key='client_version')
-                 #]
-
