846ca87c03014b5c4c50528306659f32a055f3fa
Travis Holton <travis@ideegeo.com>
2014-11-19 14:35:07 +1300

    only declare the exchange

--- rpc/queues.py
+++ rpc/queues.py
@@ -1,10 +1,13 @@
-from kombu import Exchange, Queue
+from kombu import Exchange
 
-consumer_exchange = Exchange('rabbitpy_server_ex', type='direct')
-client_exchange = Exchange('rabbitpy_client_ex', type='direct')
-client_queues = [Queue('client_hello', client_exchange, routing_key='client_hello'),
-                 Queue('client_version',
-                       client_exchange,
-                       routing_key='client_version')
-                 ]
+exchange = Exchange('rabbitpy', type='direct')
+#server_queue
+
+#consumer_exchange = Exchange('rabbitpy_server_ex', type='direct')
+#client_exchange = Exchange('rabbitpy_client_ex', type='direct')
+#client_queues = [Queue('client_hello', client_exchange, routing_key='client_hello'),
+                 #Queue('client_version',
+                       #client_exchange,
+                       #routing_key='client_version')
+                 #]
 
