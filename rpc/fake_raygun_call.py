7cf3064a6bece7ee867e75f900faa911014081ea
Travis Holton <travis@ideegeo.com>
2014-11-28 15:21:20 +1300

    add script to simulate a fake call to raygun

---
+++ rpc/fake_raygun_call.py
@@ -0,0 +1,58 @@
+import sys
+import jsonpickle
+from werkzeug.exceptions import RequestTimeout
+from kombu import Connection, Consumer
+from kombu.pools import producers
+
+from raygun4py import raygunprovider, raygunmsgs
+
+from rpc import conn_dict
+from rpc.exchange import exchange
+
+body = {
+    "class": 'TestRaygun',
+    "version": '0.0.1',
+    "user": "joe.user@ideegeo.com",
+    "tags": ["test", "rabbit"],
+    "keys": {
+        "key1": 1111,
+        "key2": 22222
+    },
+    "request": {
+        #"headers": {
+        #"Content-Type": 'application/html',
+        #},
+        #"hostName": "localhost",
+        #"httpMethod": "GET",
+        #"queryString": "",
+        #"url": "http://api.iwantmyname.com",
+        #"ipAddress": "73.23.44.19",
+    }
+}
+if __name__ == '__main__':
+    print("Running code.")
+    try:
+        raise RequestTimeout('Fake a call to the raygun task')
+    except:
+        print("Exception raised ")
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        rg_except = raygunmsgs.RaygunErrorMessage(exc_type,
+                                                  exc_value,
+                                                  exc_traceback,
+                                                  'FakeRaygun')
+        msg = raygunmsgs.RaygunMessageBuilder().new() \
+            .set_machine_name('test-machine') \
+            .set_version('v1.2.3') \
+            .set_exception_details(rg_except) \
+            .set_user(body['user']) \
+            .set_tags(body['tags']) \
+            .build()
+
+        payload = jsonpickle.encode(msg, unpicklable=False)
+        with Connection(**conn_dict) as conn:
+            with producers[conn].acquire(block=True) as producer:
+                producer.publish(payload,
+                                 serializer='json',
+                                 exchange=exchange,
+                                 declare=[exchange],
+                                 routing_key='rabbitpy.raygun')
