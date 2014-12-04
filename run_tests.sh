c9d655a3b8faf3bcbd0f72d8aebed8aafd66f72e
Joel Pitt <joel@joelpitt.com>
2014-12-04 14:04:44 +1300

    add RABBIT_API_KEY to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 # These should already exist when running tests inside the container
 export AMQ_PORT_5672_TCP_ADDR=${AMQ_PORT_5672_TCP_ADDR='192.168.33.66'}
 export AMQ_PORT_5672_TCP_PORT=${AMQ_PORT_5672_TCP_PORT='5672'}
+export RAYGUN_API_KEY=${RAYGUN_API_KEY=''}
 
 nosetests --with-doctest --with-coverage --cover-html --cover-html-dir=htmlcov "$@" tests/
