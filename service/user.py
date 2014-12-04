cb83f7bbbe3ac2755fd011b6040e9db8d9d3b0c5
Joel Pitt <joel@joelpitt.com>
2014-12-04 16:21:55 +1300

    return list of domains user has access to

---
+++ service/user.py
@@ -0,0 +1,34 @@
+import logging
+
+from kombu import Exchange
+
+from rabbit.queuerate import Queuerator
+
+from domainsage.models import User
+from domainsage.services import couch_connect
+
+logger = logging.getLogger(__name__)
+
+default_exchange = Exchange('amq.direct', type='direct')
+consumer = Queuerator(queue='api.user', exchange=default_exchange)
+
+couchdb = couch_connect('development')
+
+
+@consumer.rpc
+def domains(data):
+    """ Return list of domains a user has access to. """
+    try:
+        domains = []
+        u = User.by_email(couchdb, data.get('user'))
+    except Exception:
+        return {'domains': []}
+
+    for account in u.accounts:
+        domains.extend(list(account.domains.keys()))
+
+    return {'response': {'domains': domains}}
+
+
+if __name__ == '__main__':
+    consumer.run()
