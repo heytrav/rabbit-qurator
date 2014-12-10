01cb824ada26dafbf31f16f888be265edd854644
Travis Holton <travis@ideegeo.com>
2014-12-10 14:57:38 +1300

    split up function for testing; use specific "test" queue

--- service/account.py
+++ service/account.py
@@ -23,13 +23,21 @@ def account_for_domain(data):
     """
     logger.debug("Received query: {!r}".format(data))
     domains = data['domains']
+    return accounts_for_domains(domains)
+
+def accounts_for_domains(domains):
+    """Search for accounts for domains.
+
+    :domains: list of domains to search
+    :returns: dict with account for each domain in list
+
+    """
     account_results = Account.for_domains(couchdb, domains)
     for domain in domains:
         acct = account_results[domain]
         account_dict = dict(acct.unwrap().items())
         account_results[domain] = account_dict
     return account_results
-
 
 if __name__ == '__main__':
     account_queuerator.run()
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,10 +1,10 @@
 import unittest
 
 from domainsage.services import couch_connect
-from service.account import account_queuerator
+from rabbit.queuerate import Queuerator
+from service.account import accounts_for_domains
 
 from rpc.client import RpcClient
-from rabbit.exchange import exchange
 from tests.test_rabbit import TestRabbitpy
 
 try:
@@ -17,17 +17,27 @@ except Exception as e:
 class TestAccountService(TestRabbitpy):
 
     """Tests for account service."""
+    def setUp(self):
+        """Set up unit test stuff."""
+        TestRabbitpy.setUp(self)
 
     def test_query_with_domain(self):
         """Query the accounts db with a known domain."""
-        self._exchange = exchange
-        self.pre_declare_queues(['rabbitpy.core.account'])
-        client = RpcClient(exchange=self._exchange)
 
-        client.rpc('account_for_domain',
+        q = Queuerator(exchange=self._exchange,
+                       queue='rabbitpy.test.account')
+        self.pre_declare_queues(['rabbitpy.test.account',
+                                 'search_domains.client'])
+        @q.rpc(queue_name='rabbitpy.test.account')
+        def search_domains(data):
+            return accounts_for_domains(data['domains'])
+
+        client = RpcClient(exchange=self._exchange)
+        client.rpc('search_domains',
                    {"domains": ['iwantmyname.co.nz']},
-                   server_routing_key='rabbitpy.core.account')
-        self.pull_messages(account_queuerator, command='account_for_domain')
+                   server_routing_key='rabbitpy.test.account')
+
+        self.pull_messages(q, command='search_domains')
         for reply in client.retrieve_messages():
             self.assertIn('iwantmyname.co.nz', reply)
             account = reply['iwantmyname.co.nz']
