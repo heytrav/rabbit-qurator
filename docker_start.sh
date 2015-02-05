4d65201f2f395586055baa68e530c624d022511a
Travis Holton <travis@ideegeo.com>
2015-02-06 08:25:56 +1300

    modify to point at more generic mount points

--- docker_start.sh
+++ docker_start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-ln -s /usr/local/d8o/logstash-forwarder/logstash-forwarder /etc/init.d/logstash-forwarder
-ln -s /usr/local/d8o/logstash-forwarder/logstash-forwarder.conf /etc/supervisor/conf.d/logstash-forwarder.conf
+ln -s /usr/local/logstash-forwarder/logstash-forwarder /etc/init.d/logstash-forwarder
+ln -s /usr/local/logstash-forwarder/logstash-forwarder.conf /etc/supervisor/conf.d/logstash-forwarder.conf
 if [ "$INTERACTIVE" = 1 ]; then
     /usr/bin/supervisord -c /etc/supervisor/supervisord.conf && /bin/bash
 else
