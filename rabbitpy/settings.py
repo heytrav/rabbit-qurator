95e785724633aee214eeaed48bacc7b328079543
Travis Holton <travis@ideegeo.com>
2015-04-06 11:57:07 +1200

    setup default settings; import from local_settings.py if exists

--- rabbitpy/settings.py
+++ rabbitpy/settings.py
@@ -1,7 +1,20 @@
 
-RABBITMQ_SERVICE_HOST=None
-RABBITMQ_SERVICE_PORT=None
-RABBITMQ_USER=None
-RABBITMQ_PASSWORD=None
-RABBITMQ_VHOST=None
+import os
+RABBITMQ_SERVICE_HOST=os.environ['RABBITMQ_SERVICE_HOST']
+RABBITMQ_SERVICE_PORT=os.environ['RABBITMQ_SERVICE_PORT']
+RABBITMQ_USER=os.environ['RABBITMQ_USER']
+RABBITMQ_PASSWORD=os.environ['RABBITMQ_PASSWORD']
+RABBITMQ_VHOST=os.environ['RABBITMQ_VHOST']
 RABBITMQ_SSL=False
+
+try:
+    # TODO maybe support specific file with config instead of just path
+    # http://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
+    if 'RABBITPY_SETTINGS_DIR' in os.environ:
+        local_settings_dir = os.path.realpath(
+            os.path.expanduser(
+                os.environ['RABBITPY_SETTINGS_DIR']))
+        sys.path.insert(0, local_settings_dir)
+    from local_settings import *
+except ImportError:
+    pass
