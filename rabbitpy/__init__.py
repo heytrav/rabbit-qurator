fbb1f96dda14a4fd61eb475f54e3ea032a54cbc9
Travis Holton <travis@ideegeo.com>
2015-05-10 07:52:37 +1200

    try and get a local config into logging somehow

--- rabbitpy/__init__.py
+++ rabbitpy/__init__.py
@@ -1,18 +1,22 @@
 # main rabbitpy module
+import os
 import logging
 import logging.config
 
 from rabbitpy.settings import LOGGING_CONFIG
 
 
-def get_logger(name=None):
+def get_logger(name=None, config=None):
     """Top level logger initialiser thingy.
 
     :name: str identifying logger
     :returns: logging logger object
 
     """
+    if config is not None and name is not None:
+        LOGGING_CONFIG['loggers'][name] = config
     logging.config.dictConfig(LOGGING_CONFIG)
     logger = logging.getLogger(name)
     logger.debug("Just created logger with config {!r}".format(LOGGING_CONFIG))
+    logger.debug("Environment: %s" % os.environ.get('RABBITPY_SETTINGS_DIR'))
     return logger
