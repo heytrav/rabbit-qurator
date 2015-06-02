9c3ba6fadbc52803a54a3f86963f3fa3ea7fb438
Travis Holton <travis@ideegeo.com>
2015-06-02 14:07:57 +1200

    rename personal handle script; create one to interact with prod
    cleanup whitespace

---
+++ scripts/prod_personal_handle.py
@@ -0,0 +1,14 @@
+
+
+from kombu import Exchange
+from rabbitpy.rpc.client import RpcClient
+
+if __name__ == '__main__':
+    client = RpcClient()
+    reply = client.rpc('personal_handle',
+               {"handles": {"personal":{'id': '001bffe546d6705f4e004a80e30769bd'}}},
+               server_routing_key='api.handle')
+    print("Got reply: {!r}".format(reply))
+
+
+
