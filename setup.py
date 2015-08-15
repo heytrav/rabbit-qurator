cb3db8aeaae7c48d6cfa3b42fd2d87a363148666
Travis Holton <travis@ideegeo.com>
2015-08-16 11:19:13 +1200

    handle version setup better

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ import subprocess
 install_reqs = parse_requirements('requirements.txt', session=str(uuid.uuid4()))
 reqs = [str(ir.req) for ir in install_reqs]
 
-version = subprocess.Popen(
+raw_version = subprocess.Popen(
     "git describe",
     shell=True,
     stdout=subprocess.PIPE).stdout.read()
-version = version.decode('utf-8')
-version.strip()
+raw_version = raw_version.decode('utf-8')
+version = raw_version.strip()
 
 setup(name='rabbit-qurator',
       author='Travis Holton',
@@ -18,7 +18,7 @@ setup(name='rabbit-qurator',
       author_email='wtholton@gmail.com',
       description='Create RabbitMQ endpoints using decorators.',
       long_description='Create RabbitMQ endpoints for RPC and tasks using decorators based on kombu.',
-      version='v1.0.2',
+      version=version,
       packages=['qurator',
                 'qurator.rpc',
                 'qurator.tests'])
