14522838030d3a01dcb4465c2c4d2a6d95ec6e5e
Travis Holton <travis@ideegeo.com>
2015-09-23 08:19:46 +1200

    manually set version

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 import uuid
-import subprocess
 from distutils.core import setup
 from pip.req import parse_requirements
 install_reqs = parse_requirements('requirements.txt', session=str(uuid.uuid4()))
 reqs = [str(ir.req) for ir in install_reqs]
-
-raw_version = subprocess.Popen(
-    "git describe",
-    shell=True,
-    stdout=subprocess.PIPE).stdout.read()
-raw_version = raw_version.decode('utf-8')
-version = raw_version.strip()
 
 setup(name='rabbit-qurator',
       author='Travis Holton',
@@ -19,7 +11,7 @@ setup(name='rabbit-qurator',
       install_requires=reqs,
       description='Create RabbitMQ endpoints using decorators.',
       long_description='Create RabbitMQ endpoints for RPC and tasks using decorators based on kombu.',
-      version=version,
+      version='1.2.8',
       packages=['qurator',
                 'qurator.rpc',
                 'qurator.tests'])
